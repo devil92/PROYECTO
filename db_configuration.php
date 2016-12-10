95c38feb324220bbc6877a7156e85a14cf63ca83
devil92 <manuel.lv92@gmail.com>
2016-12-10 16:33:29 +0100

    Update db_configuration.php

--- db_configuration.php
+++ db_configuration.php
@@ -4,7 +4,7 @@
     $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
     $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
     $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
-    $db_name=$dbname;
+    $db_name=$_ENV['OPENSHIFT_MYSQL_DB_NAME']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
   } else {
     $db_user="root"; //my db user
     $db_host="localhost"; //my db host
