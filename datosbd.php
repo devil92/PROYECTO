fe26fd8933ae624a612ee2a5a998125f698c8671
devil92 <manuel.lv92@gmail.com>
2016-12-10 16:05:42 +0100

    Add files via upload

---
+++ datosbd.php
@@ -0,0 +1,6 @@
+<?php
+$dbname='proyecto';
+$dbuser='merino';
+$dbpassword='1234';
+$dbhost='localhost';
+?>
--- db_configuration.php
+++ db_configuration.php
@@ -1,6 +1,23 @@
 <?php
-$db_user='root';
-$db_password='';
-$db_host='localhost';
-$db_name='forololo';
+    include("datosdb.php");
+  //Checking if we are into the OpenShift App
+  if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
+    $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
+    $db_host=$_ENV['OPENSHIFT_MYSQL_DB_HOST']; //Openshift db host OPENSHIFT_MYSQL_DB_HOST
+    $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
+    $db_name=$dbname;
+  } else {
+    $db_user=$dbuser; //my db user
+    $db_host=$dbhost; //my db host
+    $db_password=$dbpassword; //my db password
+    $db_name=$dbname; //my db name
+  }
+  //CREATING THE CONNECTION
+     $connection = new mysqli($db_host,$db_user,$db_password,$db_name);
+     $connection->set_charset("utf8");
+     //TESTING IF THE CONNECTION WAS RIGHT
+     if ($connection->connect_errno) {
+         printf("Connection failed: %s\n", $connection->connect_error);
+         exit();
+     }
 ?>
