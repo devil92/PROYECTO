bc0168e294426a8a6099541429ae13260370b0db
devil92 <manuel.lv92@gmail.com>
2016-12-10 16:10:01 +0100

    Update db_configuration.php

--- db_configuration.php
+++ db_configuration.php
@@ -1,5 +1,4 @@
 <?php
-    include("datosdb.php");
   //Checking if we are into the OpenShift App
   if (isset($_ENV['OPENSHIFT_APP_NAME'])) {
     $db_user=$_ENV['OPENSHIFT_MYSQL_DB_USERNAME']; //Openshift db name OPENSHIFT_MYSQL_DB_USERNAME
@@ -7,10 +6,10 @@
     $db_password=$_ENV['OPENSHIFT_MYSQL_DB_PASSWORD']; //Openshift db password OPENSHIFT_MYSQL_DB_PASSWORD
     $db_name=$dbname;
   } else {
-    $db_user=$dbuser; //my db user
-    $db_host=$dbhost; //my db host
-    $db_password=$dbpassword; //my db password
-    $db_name=$dbname; //my db name
+    $db_user="root"; //my db user
+    $db_host="localhost"; //my db host
+    $db_password=""; //my db password
+    $db_name="forololo"; //my db name
   }
   //CREATING THE CONNECTION
      $connection = new mysqli($db_host,$db_user,$db_password,$db_name);
